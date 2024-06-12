02d1464db79dd401bf635f73863fcd382952594d
61018648 <61018648@qq.com>
2024-06-12 22:10:13 +0800

    服务端代码

---
+++ src/main.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+const server = http.createServer((request,response)=>{
+    const data = {
+        id:1,
+        title:'关山月',
+        content:'明月出天山'
+    }
+    const jsonData = JSON.stringify(data);
+    response.writeHead(200,{
+        'Content-Type':'application/json; charset=utf-8'
+    })
+    response.write(jsonData);
+    response.end();
+})
+
+server.listen(3000,()=>{
+    console.log('服务已启动')
+})
